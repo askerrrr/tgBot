26acf66287ad2fcf01a3138273a941684c31133f
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-07-30 12:43:14 +0300

    отправка документа админу

--- src/services/order/multipleOrders.js
+++ src/services/order/multipleOrders.js
@@ -1,7 +1,15 @@
 async function multipleOrders(conversation, ctx) {
-  await ctx.reply("Пришлите документ с вашими заказами");
-  const { orderDocument } = await conversation.wait();
-  await ctx.reply("Скоро начнем обрабатывать ваши заказы!");
+  await ctx.reply("Пришлите документ с вашими товарами");
+  const { message } = await conversation.waitFor("message:document");
+  const document = message.document;
+  const fileId = document.file_id;
+
+  await ctx.reply("Документ получили, а теперь напишите номер вашего телефона");
+  const userPhoneNumber = await conversation.wait();
+
+  await ctx.reply("Спасибо, скоро мы займемся вашим заказом!");
+  await ctx.api.sendDocument("5364121551", fileId);
+  await ctx.api.sendMessage("5364121551", userPhoneNumber.msg.text);
 }
 
-module.exports = { multipleOrders };
+module.exports = { multipleOrders }; //экспорт в src\middleware\middleware.js

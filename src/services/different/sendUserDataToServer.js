34512ffc4c020b2a137574727976c287fab2d761
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-07-31 11:59:48 +0300

    работа с авторизацией

---
+++ src/services/different/sendUserDataToServer.js
@@ -0,0 +1,14 @@
+const env = require("../../../env");
+
+module.exports.sendUserDataToServer = async (userData) => {
+  const response = await fetch("/", {
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: Bearer`${env.auttoken}`,
+    },
+  });
+  const users = await response.json();
+  return users;
+};

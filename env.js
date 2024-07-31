fdd57acdb82afed5f8ed96ab40ff14ae913933c5
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-07-31 15:20:58 +0300

    работа с авторизацией

--- env.js
+++ env.js
@@ -13,4 +13,11 @@ Object.defineProperty(env, "bottoken", {
   configurable: false,
 });
 
+Object.defineProperty(env, "auttoken", {
+  value:
+    "v?4eKHqGFYm2aV4F?UQUZ5uPxHThrm?uuFbuJP!Rm3RHzXbKRb4ggHVnzSuOtmrI-gZGMJCw2LSsHKuj53C1DwrUa?mgMd5BqcmaVowwZcDB4CHuaE?=X6on69jg4GahbyS/Z2W1sxEw0GPa?RA145sSdN0ZxlAljnSME6g==Su!puN9CmTECntQY8o/5CsmzdJ1CuUhs03h8k/!qsBT5-9YIPK5oWdJCeAns18e9Ghm?!b8iC0phZg2E4BhYFMR",
+  writable: false,
+  configurable: false,
+});
+
 module.exports = env;
--- src/chatMember/chatMember.js
+++ src/chatMember/chatMember.js
@@ -1,44 +1,49 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const env = require("../../env");
-const e = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const sendUserDataToServer = require("../services/different/sendUserDataToServer");
 
-const client = new MongoClient(env.mongourl, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// const client = new MongoClient(env.mongourl, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
 
-(async () => {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } catch (err) {
-    console.log(err);
-  }
-})();
+// (async () => {
+//   try {
+//     await client.connect();
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
 
-const collection = client.db("TelegramUsers").collection("users");
+//const collection = client.db("TelegramUsers").collection("users");
 
 module.exports.chat = async (bot) => {
   bot.hears("/start", async (ctx) => {
-    await ctx.reply(
-      `${ctx.from.first_name}, добро пожаловать в наш бот  🤖\nВам присвоен id-${ctx.chat.id}, в дальнейшем этот ID будет соответствовать номеру вашего заказа\nДля дальнейшей работы воспользуйтесь 'Меню' `
-    );
+    try {
+      await ctx.reply(
+        `${ctx.from.first_name}, добро пожаловать в наш бот  🤖\nВам присвоен id-${ctx.chat.id}, в дальнейшем этот ID будет соответствовать номеру вашего заказа\nДля дальнейшей работы воспользуйтесь 'Меню' `
+      );
 
-    const chatMember = await ctx.chatMembers.getChatMember(
-      ctx.chat.id,
-      ctx.from.id
-    );
-    const existingDocument = await collection.findOne(chatMember.user);
-    if (!existingDocument) {
-      return await collection.insertOne(chatMember.user);
-    } else {
-      console.log("Document already exists in the collection");
+      const chatMember = await ctx.chatMembers.getChatMember(
+        ctx.chat.id,
+        ctx.from.id
+      );
+      //const existingDocument = await collection.findOne(chatMember.user);
+      if (!existingDocument) {
+        return await collection.insertOne(chatMember.user);
+      } else {
+        console.log("Document already exists in the collection");
+      }
+      await sendUserDataToServer(chatMember.user);
+    } catch (err) {
+      console.log(err);
     }
   });
 };

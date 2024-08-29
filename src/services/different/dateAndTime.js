e3036358c4efcea8bf2a476c557ffd02707e5288
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-08-29 17:43:20 +0300

    visual changes func getDateAndTime

--- src/services/different/dateAndTime.js
+++ src/services/different/dateAndTime.js
@@ -3,12 +3,17 @@ function getDateAndTime() {
 
   const second =
     date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
   const minutes =
     date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+
   const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+
   const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+
   const month =
     date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+
   const year = date.getFullYear();
 
   return {

72f3c737754a50a67a340452bdb5bfa7c82ebee2
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-08-29 17:44:37 +0300

    visual changes func getCNYValuteValue

--- src/services/orderCost/service/getCNYValuteValue.js
+++ src/services/orderCost/service/getCNYValuteValue.js
@@ -1,8 +1,8 @@
-
-
 async function getCNYValuteValue() {
   let res = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
+
   let valute = await res.json();
+
   return valute.Valute.CNY.Value;
 }
 

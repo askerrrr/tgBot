e374f8464de164c39387e71c64a76df6805f70a4
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-08-30 12:23:30 +0300

    visual change

--- src/services/orderCost/calcOrderCost.js
+++ src/services/orderCost/calcOrderCost.js
@@ -4,11 +4,11 @@ async function calcOrderCost(conversation, ctx) {
   await ctx.reply("Отправьте число");
   let response = await conversation.wait();
   let num = response.msg.text;
-  let val = await convertYuanToRubles(num);
+  let result = await convertYuanToRubles(num);
 
   if (num > 0) {
     await ctx.reply(
-      `Ориентировочная стоимость товара ${val} рублей \n*без учета стоимости доставки`
+      `Ориентировочная стоимость товара ${result} рублей \n*без учета стоимости доставки`
     );
   } else {
     await ctx.reply(`Введите числовое значение, которое больше 0`);

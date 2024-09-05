96d36e42fa70720e6f733be085ff128120397e44
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-09-05 09:03:54 +0300

    rm await

--- src/listeners/allLinteners.js
+++ src/listeners/allLinteners.js
@@ -10,14 +10,13 @@ const { backToOtherMenu } = require("./differentListeners/backToOtherMenu");
 const { middlewareForConversations } = require("../middleware/middleware");
 
 async function allListeners(bot) {
-  await guides(bot);
-  await getFileId(bot);
-  await backToMainMenu(bot);
-  await backToOtherMenu(bot);
+  guides(bot);
+  getFileId(bot);
+  backToOtherMenu(bot);
   listenersForApp(bot);
-  await listenersForOther(bot);
-  await listenersOfMainMenu(bot);
-  await middlewareForConversations(bot);
+  listenersForOther(bot);
+  listenersOfMainMenu(bot);
+  middlewareForConversations(bot);
 }
 
 module.exports = { allListeners };

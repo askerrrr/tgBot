9476f442fa82aa7b0f671a9db725a832ced0d11d
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-09-09 14:49:02 +0300

    add new func sendOrderInfoToServer

---
+++ src/services/order/singleOrder/services/sendOrderInfoToServer.js
@@ -0,0 +1,29 @@
+const { env } = require("../../../../../env");
+
+module.exports.sendOrderInfoToServer = async (data) => {
+  try {
+    const response = await fetch("/url", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${env.auth_token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Server error: ${response.status} ${response.statusText}`
+      );
+    }
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      return await response.json();
+    } else {
+
+      const text = await response.text();
+      throw new Error(`Unexpected content type: ${contentType}\n${text}`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};

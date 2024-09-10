c4c3a7a8db296805d8d22910a00644f05418e39c
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-09-10 20:32:20 +0300

    add new func convertImageToJSON

---
+++ src/services/order/singleOrder/services/convertImageToJSON.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+async function convertImageToJSON(image) {
+  const imgInput = fs.readFileSync(image);
+
+  const base64img = imgInput.toString("base64");
+
+  const jsonObject = {
+    imgInput: base64img,
+  };
+  return jsonObject;
+}
+
+module.exports = { convertImageToJSON };

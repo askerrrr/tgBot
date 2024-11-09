1813ef7a57da97aa93592a145e44179355ee2328
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-09 11:50:34 +0300

    https => http, add port

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("https://62.109.30.45", async (req, res) => {
+app.post("http://62.109.30.45:3000", async (req, res) => {
   try {
     const requestPayload = req.body;
 

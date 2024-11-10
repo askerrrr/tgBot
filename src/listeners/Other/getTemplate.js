17161352d50e06206d1f58c8edcbbe9b96b7c3c2
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-10 19:45:46 +0300

    change template path

--- src/listeners/Other/getTemplate.js
+++ src/listeners/Other/getTemplate.js
@@ -3,7 +3,7 @@ const { InputFile } = require("grammy");
 async function getTemplate(bot) {
   bot.hears("Получить шаблон", async (ctx) => {
     await ctx.replyWithDocument(
-      new InputFile("/home/testbot/tgbot/srs/utils/шаблон.xlsx")
+      new InputFile("/home/testbot/tgbot/srs/utils/template.xlsx")
     );
   });
 }

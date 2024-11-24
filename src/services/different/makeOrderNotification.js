2a283a7d15f750283faa7081e67f319fdd6a0030
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-24 10:51:50 +0300

    corrected file path in makeOrderNotification

--- src/services/different/makeOrderNotification.js
+++ src/services/different/makeOrderNotification.js
@@ -1,13 +1,9 @@
+const { env } = require("../../../env");
+
 module.exports.makeOrderNotification = (order) => {
   if (order.type) {
-    return `Новый заказ\n\nID пользователя : ${order.userId}\nНомер телефона : ${order.phone}\nСсылка на товар : ${order.file.path}\nОписание :\nКоличество : ${order.description.quantity}\nРазмер : ${order.description.size}\nID заказа : ${order.id}\n Время заказа : ${order.date}`;
+    return `Новый заказ\n\nID пользователя : ${order.userId}\nНомер телефона : ${order.phone}\nСсылка на товар : ${env.main_server}/download/${order.userId}/${order.id}\nОписание :\nКоличество : ${order.description.quantity}\nРазмер : ${order.description.size}\nID заказа : ${order.id}\n Время заказа : ${order.date}`;
   }
 
-  return `Новый заказ\n\nID пользователя: ${order.userId}\nНомер телефона : ${
-    order.phone
-  }\nСсылка на таблицу : ${
-    order.file.path
-  }\nСсылка на файл : ${""}\nВремя заказа : ${order.date}\nID заказа : ${
-    order.id
-  }`;
+  return `Новый заказ\n\nID пользователя: ${order.userId}\nНомер телефона : ${order.phone}\nСсылка на таблицу : ${order.file.path}\nСсылка на файл : ${env.main_server}/download/${order.userId}/${order.id} \nВремя заказа : ${order.date}\nID заказа : ${order.id}`;
 };

31b7aae01ba3455ea953386c5c0cbb8bf8f6cfd5
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-24 10:42:20 +0300

    corrected makeOrderNotification

--- src/services/different/makeOrderNotification.js
+++ src/services/different/makeOrderNotification.js
@@ -1,7 +1,13 @@
 module.exports.makeOrderNotification = (order) => {
   if (order.type) {
-    return `Новый заказ\n\nID пользователя : ${order.userId}\nНомер телефона : ${order.phone}\nСсылка на товар : ${order.file.url}\nОписание :\nКоличество : ${order.file.description.quantity}\nРазмер : ${order.file.description.size}\nID заказа : ${order.file.id}\n Время заказа : ${order.date}`;
+    return `Новый заказ\n\nID пользователя : ${order.userId}\nНомер телефона : ${order.phone}\nСсылка на товар : ${order.file.path}\nОписание :\nКоличество : ${order.description.quantity}\nРазмер : ${order.description.size}\nID заказа : ${order.id}\n Время заказа : ${order.date}`;
   }
 
-  return `Новый заказ\n\nID пользователя: ${order.userId}\nНомер телефона : ${ order.phone}\nСсылка на таблицу : ${ order.file.url}\nСсылка на файл : ${""}\nВремя заказа : ${order.date}\nID заказа : ${ order.file.id}`;
+  return `Новый заказ\n\nID пользователя: ${order.userId}\nНомер телефона : ${
+    order.phone
+  }\nСсылка на таблицу : ${
+    order.file.path
+  }\nСсылка на файл : ${""}\nВремя заказа : ${order.date}\nID заказа : ${
+    order.id
+  }`;
 };

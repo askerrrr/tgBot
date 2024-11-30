bbd1567a2b4612303590bec636fe7a7a3ee0608a
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-30 21:03:05 +0300

    check numbers of active orders

--- src/listeners/MainMenu/order.js
+++ src/listeners/MainMenu/order.js
@@ -1,7 +1,18 @@
 var { keyboardForOrder } = require("../../keyboard/keyboard");
+var { findOrder } = require("../../database/services/findOrder");
 
 module.exports.order = async (bot) => {
   bot.hears("Сделать заказ!", async (ctx) => {
+    var userId = ctx.chat.id;
+
+    var activeOrders = await (await findOrder(userId)).active();
+
+    if (activeOrders && activeOrders.length > 5) {
+      await ctx.reply("Вы превысили количество активных заказов");
+
+      return;
+    }
+
     await ctx.reply("Выберите один из вариантов", {
       reply_markup: keyboardForOrder,
     });

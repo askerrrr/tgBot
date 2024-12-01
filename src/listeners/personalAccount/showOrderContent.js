f91bf2f0c6a8d246f44f22aa6d20e6daed59f4bf
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-01 15:25:44 +0300

    corrected showOrderContent

--- src/listeners/personalAccount/showOrderContent.js
+++ src/listeners/personalAccount/showOrderContent.js
@@ -2,10 +2,10 @@ const { statusTranslate } = require("../../services/different/statusTranslate");
 
 module.exports.showOrderContent = (order, userId) => {
   return `ID пользователя : ${userId}\nID заказа : ${
-    order.file.id
+    order.id
   }\nНомер телефона : ${order.phone}\nВремя заказа ${
     order.date
   }\nСтатус заказа : ${statusTranslate(
-    order.file.status
+    order.orderStatus
   )}\n\nПосмотреть содержимое заказа можно по ссылке`;
 };

47245c30114e6c16488dae3ed01fb98132dc4076
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-17 20:29:56 +0300

    refactor checkPhoneNumber

--- src/services/order/services/checkPhoneNumber.js
+++ src/services/order/services/checkPhoneNumber.js
@@ -1,7 +1,4 @@
-module.exports.checkPhoneNumber = (phone) => {
-  return (
-    String(phone).length < 12 &&
-    String(phone).length > 10 &&
-    typeof phone === "number"
-  );
-};
+module.exports.checkPhoneNumber = (phone) =>
+  String(phone).length < 12 &&
+  String(phone).length > 10 &&
+  typeof phone === "number";

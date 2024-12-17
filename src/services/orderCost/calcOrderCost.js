45946fc8aad9beea077f9acf0a7d25e68084cea8
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-17 21:52:54 +0300

    refactor calcOrderCost

--- src/services/orderCost/calcOrderCost.js
+++ src/services/orderCost/calcOrderCost.js
@@ -3,16 +3,16 @@ var { convertYuanToRubles } = require("./service/convertYuanToRubles");
 async function calcOrderCost(conversation, ctx) {
   await ctx.reply("Отправьте число");
 
-  var response = await conversation.wait();
+  var result = await conversation.wait();
 
-  var num = response.msg.text;
+  var num = result.msg.text;
 
-  if (num > 0 && num < 1000000) {
+  if (num > 0 && num < 1e6) {
     var result = await convertYuanToRubles(num);
     await ctx.reply(
       `Ориентировочная стоимость товара ${result}р \n*без учета стоимости доставки\n\nПодробнее о тарифах доставки можно узнать в разделе "Часто задаваемые вопросы"`
     );
-  } else if (num > 1000000) {
+  } else if (num > 1e6) {
     await ctx.reply(
       `Боюсь, что у вас нет таких денег)))\nВведите числовое значение, которое больше 0`
     );

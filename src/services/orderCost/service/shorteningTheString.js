e26eaf28f919090f3a0060ec2e8731c890227224
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-21 23:14:16 +0300

    refactor shorteningTheString

--- src/services/orderCost/service/shorteningTheString.js
+++ src/services/orderCost/service/shorteningTheString.js
@@ -1,18 +1,5 @@
-module.exports.shorteningTheString = async (val) => {
-  var arr = [],
-    arr1 = [];
+module.exports.shorteningTheString = async (value) => {
+  var [a, b] = value.split(".");
 
-  var a = val.split("");
-  var b = val.split("").reverse();
-
-  for (var i = 0; i < a.length; i++) {
-    if (a[i].includes(".")) break;
-    arr.push(a[i]);
-
-    if (b[i].includes(".")) break;
-    arr1.push(b[i]);
-  }
-
-  val = arr.join("") + "." + arr1.reverse().slice(0, 2).join("");
-  return val;
+  return a + "." + b.slice(0, 2);
 };
